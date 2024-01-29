1e78067812df7cc8b49d8df4e0f58303f5ee439e
txart <iurzainki@gmail.com>
2024-01-29 20:04:01 +0100

    get first word of line

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,34 @@ struct Cli {
     // path: std::path::PathBuf,
 }
 
-fn main() {
-    // let args = Cli::parse();
+// constants
+// Maybe move to config file?
+const TIMESTAMPS_FILENAME: &str = "timestamps.dat";
 
+fn read_first_line_from_file(filename: &str) -> String {
     // Read first line of file
-    let f = File::open("timestamps.dat").expect("timestamps file not found!");
+    let f = File::open(filename).expect("file not found!");
     let mut reader = BufReader::new(f);
 
     let mut line = String::new();
-    reader.read_line(&mut line).expect("Failed to read line!");
-    println!("First line is {:?}", line);
+    reader
+        .read_line(&mut line)
+        .expect("Failed to read first line!");
+
+    line
+}
+
+fn main() {
+    // let args = Cli::parse();
+
+    let first_line: String = read_first_line_from_file(TIMESTAMPS_FILENAME);
+
+    // let first_word = match first_line.split(' ').next() {
+    //     Some(value) => Some(value),
+    //     None => None,
+    // };
+
+    let first_word: Option<&str> = first_line.split(' ').next();
+
+    println!("{:?}", first_word);
 }
