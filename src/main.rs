9416b78c1c2063dda252e574906f9dd4db62f6d3
txart <iurzainki@gmail.com>
2024-02-20 19:21:14 +0100

    first working version

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::fs::OpenOptions;
 use std::io::prelude::*;
 use std::io::BufReader;
 
@@ -16,9 +17,12 @@ struct Cli {
 
 // constants
 // Maybe move to config file?
-const TIMESTAMPS_FILENAME: &str = "timestamps.dat";
+const STATUS_FILENAME: &str = "status.txt"; // current timestamp status
+const TIMESTAMPS_FILENAME: &str = "timestamps.dat"; // historical timestamps
 const TIMESTAMP_FORMAT: &str = "%d/%m/%Y--%H:%M:%S";
 const OPEN_TIMESTAMP_KEYWORD: &str = "OPEN";
+const CLOSED_TIMESTAMP_KEYWORD: &str = "CLOSED";
+const MINIMUM_WORK_DURATION_MINUTES: i64 = 30;
 
 #[derive(Debug)]
 enum Timestamp {
@@ -49,35 +53,91 @@ fn get_two_words_from_line(line: &str) -> [&str; 2] {
     [first, second]
 }
 
-fn read_last_timestamp() -> Timestamp {
-    let timestamps_first_line: String = read_first_line_from_file(TIMESTAMPS_FILENAME);
-    let [status, timestamp] = get_two_words_from_line(&timestamps_first_line);
+fn read_timestamp_from_string(s: &str) -> NaiveDateTime {
+    NaiveDateTime::parse_from_str(s, TIMESTAMP_FORMAT)
+        .expect("Error reading latest timestamp from file!")
+}
+
+fn get_current_status() -> Timestamp {
+    let status_line: String = read_first_line_from_file(STATUS_FILENAME);
+    let [status, timestamp] = get_two_words_from_line(&status_line);
 
     if status == OPEN_TIMESTAMP_KEYWORD {
-        let timestamp = NaiveDateTime::parse_from_str(timestamp, TIMESTAMP_FORMAT)
-            .expect("Error reading latest timestamp from file!");
+        let timestamp = read_timestamp_from_string(timestamp);
         Timestamp::Open(timestamp)
-    } else {
+    } else if status == CLOSED_TIMESTAMP_KEYWORD {
         Timestamp::Closed
+    } else {
+        panic!("Cannot read status from status.txt file. File corrupted.")
     }
+}
+
+fn get_current_datetime() -> NaiveDateTime {
+    Utc::now().naive_local()
+}
+
+fn datetime_to_string(dt: NaiveDateTime) -> String {
+    dt.format(TIMESTAMP_FORMAT).to_string()
+}
+
+fn append_line_to_file(line: &str, filename: &str) {
+    let mut file = OpenOptions::new().append(true).open(filename).unwrap();
+
+    writeln!(file, "{}", line).unwrap();
+}
+
+fn write_line_to_file(line: &str, filename: &str) {
+    //overwrites all file contents!
+    std::fs::write(line, filename).unwrap();
+}
+
+fn set_timestamp_status_open() {
+    let line_to_write: String =
+        OPEN_TIMESTAMP_KEYWORD.to_owned() + " " + &datetime_to_string(get_current_datetime());
+
+    write_line_to_file(STATUS_FILENAME, line_to_write.as_str())
+}
+
+fn minutes_since_last_timestamp(ts: NaiveDateTime) -> i64 {
+    Utc::now()
+        .naive_local()
+        .signed_duration_since(ts)
+        .num_minutes()
+}
+
+fn set_timestamp_status_closed() {
+    let line: String = CLOSED_TIMESTAMP_KEYWORD.to_owned() + " TIMESTAMP";
+    write_line_to_file(STATUS_FILENAME, &line)
+}
+
+fn add_timestamp_to_history(timestamp_duration: i64) {
+    let line_to_write: String =
+        CLOSED_TIMESTAMP_KEYWORD.to_owned() + " " + &timestamp_duration.to_string();
+    append_line_to_file(&line_to_write, TIMESTAMPS_FILENAME);
 }
 
 fn main() {
     // let args = Cli::parse();
 
-    let last_timestamp = read_last_timestamp();
+    let last_timestamp = get_current_status();
     println!("Last timestamp: {:?}", last_timestamp);
 
     match last_timestamp {
         Timestamp::Open(ts) => {
-            let dur = Utc::now().naive_local().signed_duration_since(ts);
-            println!(
-                "Time passed since last open timestamp: {} minutes",
-                dur.num_minutes()
-            );
+            let dur = minutes_since_last_timestamp(ts);
+
+            if dur < MINIMUM_WORK_DURATION_MINUTES {
+                println!(
+                    "Not enough time has passed. You have been working for only {} minutes",
+                    dur
+                )
+            } else {
+                set_timestamp_status_closed();
+                add_timestamp_to_history(dur);
+            }
         }
         Timestamp::Closed => {
-            println!("TODO: Open new timestamp!");
+            set_timestamp_status_open();
         }
     }
 }
