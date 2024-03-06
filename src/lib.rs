865c28a0913629988837af3562dbb5758671273a
txart <iurzainki@gmail.com>
2024-03-06 11:25:17 +0100

    added printing progress!

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,7 @@ enum TimestampStatus {
     Closed,
 }
 
+#[derive(Debug)]
 struct Timestamp {
     date: NaiveDate,
     duration: i64,
@@ -29,10 +30,13 @@ mod files_io {
     use std::io::{prelude::*, BufRead, BufReader};
     use std::path::PathBuf;
 
+    pub fn read_file_into_buffer(filename: &PathBuf) -> BufReader<File> {
+        let file = File::open(filename).expect("file not found!");
+        BufReader::new(file)
+    }
+
     pub fn read_first_line_from_file(filename: &PathBuf) -> String {
-        // Read first line of file
-        let f = File::open(filename).expect("file not found!");
-        let mut reader = BufReader::new(f);
+        let mut reader = read_file_into_buffer(filename);
 
         let mut line = String::new();
         reader
@@ -55,6 +59,8 @@ mod files_io {
 }
 
 mod timestamping {
+    use std::io::BufRead;
+
     use crate::config;
     use crate::files_io;
     use crate::Timestamp;
@@ -71,7 +77,7 @@ mod timestamping {
         let [status, datetime_string] = get_two_words_from_line(&status_line);
 
         if status == config::OPEN_TIMESTAMP_KEYWORD {
-            let datetime = read_datetime_from_string(datetime_string);
+            let datetime = datetime_from_string(datetime_string);
             TimestampStatus::Open(datetime)
         } else if status == config::CLOSED_TIMESTAMP_KEYWORD {
             TimestampStatus::Closed
@@ -103,9 +109,13 @@ mod timestamping {
         [first, second]
     }
 
-    fn read_datetime_from_string(s: &str) -> NaiveDateTime {
+    fn datetime_from_string(s: &str) -> NaiveDateTime {
         NaiveDateTime::parse_from_str(s, config::DATETIME_FORMAT)
-            .expect("Error reading latest timestamp from string")
+            .expect("Error parsing datetime from string")
+    }
+
+    fn date_from_string(s: &str) -> NaiveDate {
+        NaiveDate::parse_from_str(s, config::DATE_FORMAT).expect("Error parsing date from string")
     }
 
     fn get_current_datetime() -> NaiveDateTime {
@@ -135,17 +145,33 @@ mod timestamping {
     }
 
     fn compute_working_hours_in_a_day(day: NaiveDate) -> i64 {
-        //TODO
-        // timestamps = get_all_timestamps_in_a_day()
-        // sum_minutes_in_timestamps(timestamps)
+        let timestamps: Vec<Timestamp> = get_all_timestamps_in_a_day_from_file(day);
+        sum_minutes_in_timestamps(timestamps)
     }
 
-    fn get_all_timestamps_in_a_day(day: NaiveDate) -> Vec<Timestamp> {
-        //TODO
-        //  - read all lines in timestamps.dat
-        //  - Filter those whose date matches today's NaiveDate
-        //  - Create a Timestamp(Naiveedate, duration) with each of those
-        //  - Append them to a Vec<Timestamp>
+    fn get_timestamp_from_line(line: String) -> Timestamp {
+        let binding = line.to_string();
+        let [date, duration] = get_two_words_from_line(&binding);
+
+        Timestamp {
+            date: date_from_string(date),
+            duration: duration
+                .parse::<i64>()
+                .expect("could not parse number from string"),
+        }
+    }
+
+    fn get_all_timestamps_in_a_day_from_file(day: NaiveDate) -> Vec<Timestamp> {
+        let reader = files_io::read_file_into_buffer(&config::TIMESTAMPS_FILENAME);
+
+        let mut timestamps_of_today: Vec<Timestamp> = Vec::new();
+        for line in reader.lines() {
+            let line_ts = get_timestamp_from_line(line.expect("could not parse line!"));
+            if day == line_ts.date {
+                timestamps_of_today.push(line_ts);
+            }
+        }
+        timestamps_of_today
     }
 
     fn sum_minutes_in_timestamps(timestamps: Vec<Timestamp>) -> i64 {
@@ -154,6 +180,31 @@ mod timestamping {
             total_minutes += ts.duration
         }
         total_minutes
+    }
+
+    fn create_progress_bar(completed: usize, total: usize) -> String {
+        let completed_part: String = "=".repeat(completed - 1);
+        let remaining_part: String = "-".repeat(total - completed);
+
+        "[".to_string() + &completed_part + ">" + &remaining_part
+    }
+
+    fn show_progress_bar(progress: f64, goal: f64, bar_char_length: usize) {
+        let completed: usize = (progress * bar_char_length as f64 / goal).round() as usize;
+
+        let progress_bar = create_progress_bar(completed, bar_char_length);
+
+        println!("{}", progress_bar);
+    }
+
+    pub fn show_progress_for_today() {
+        let today = get_date_of_today();
+        let working_hours = compute_working_hours_in_a_day(today);
+        let goal = config::DAILY_WORK_GOAL_MINUTES;
+
+        show_progress_bar(working_hours as f64, goal as f64, 20);
+
+        println!("{} minutes out of {} worked today.\n", working_hours, goal);
     }
 
     #[cfg(test)]
@@ -181,12 +232,25 @@ mod timestamping {
             let timestamps = vec![ts1, ts2];
             assert_eq!(sum_minutes_in_timestamps(timestamps), 70)
         }
+
+        #[test]
+        fn gets_timestamp_from_line() {
+            let line: String = String::from("21/02/2024 67");
+            let ts = get_timestamp_from_line(line);
+
+            let date: NaiveDate = date_from_string("21/02/2024");
+
+            assert_eq!(date, ts.date);
+            assert_eq!(67, ts.duration);
+        }
     }
 }
 
 pub fn run() {
     // let args = Cli::parse();
     let last_timestamp_status = crate::timestamping::get_current_status();
+
+    crate::timestamping::show_progress_for_today();
 
     match last_timestamp_status {
         TimestampStatus::Open(datetime) => {
